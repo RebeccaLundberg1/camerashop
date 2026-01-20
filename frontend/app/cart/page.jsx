3ea6db8bc5f2d26780b6a5fc46ec66f41c71cf27
Samuel Ryberg <samuel.ryberg@gmail.com>
2026-01-20 13:47:09 +0100

    test commit

--- frontend/app/cart/page.jsx
+++ frontend/app/cart/page.jsx
@@ -1,7 +1,8 @@
 export default function CartPage({ items }) {
-    return (
-        <div className="p-6 max-w-3xl mx-auto">
-            <h1 className="text-3xl font-bold mb-6">Kundkorg</h1>
-        </div>
-    );
+  return (
+    <div className="p-6 max-w-3xl mx-auto">
+      <h2 className="text-3xl font-bold mb-6">Kundkorg</h2>
+    </div>
+  );
 }
+
