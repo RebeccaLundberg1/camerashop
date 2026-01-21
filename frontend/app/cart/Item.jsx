03b8701c978980014381043586d4c472838633cf
RebeccaLundberg1 <rebecca93lundberg@gmail.com>
2026-01-21 13:54:57 +0100

    Updated CartItem and use mockitem in page to view products, see comments when mockitems is not needed any more

--- frontend/app/cart/Item.jsx
+++ frontend/app/cart/Item.jsx
@@ -1,5 +1,43 @@
 "use client";
 
-export default function CartItem({ product }) {
+import Image from "next/image";
+import {useEffect, useState} from "react";
 
+export default function CartItem({ item }) {
+    const safeName = item.id;
+
+    const initialSrc = safeName
+        ? `/product-images/${encodeURIComponent(item.category)}/${encodeURIComponent(safeName)}.jpg`
+        : "/product-images/camera.jpg";
+
+    const [imgSrc, setImgSrc] = useState(initialSrc);
+
+    useEffect(() => {
+        setImgSrc(initialSrc);
+    }, [initialSrc]);
+
+    return (
+        <div className="p-2 flex bg-white shadow rounded w-full gap-4">
+            <div className="relative h-30 w-30">
+                <Image
+                    src={imgSrc}
+                    alt={safeName ?? 'Product'}
+                    fill
+                    className="object-cover"
+                    unoptimized
+                    onError={() => {
+                        setImgSrc("/product-images/camera.jpg");
+                    }}
+                />
+            </div>
+            <div className="flex-1 flex flex-col justify-center gap-1">
+                <h2 className="text-xl font-bold ">
+                    {item.brand} {item.model}
+                </h2>
+                <p className="text-xl font-normal">
+                    {item.price} SEK
+                </p>
+            </div>
+        </div>
+    )
 }
--- frontend/app/cart/page.jsx
+++ frontend/app/cart/page.jsx
@@ -4,7 +4,15 @@ import ProductCard from "@/app/components/productcard";
 export default async function CartPage() {
     const url = `${process.env.BACKEND_API_URL}/cart`;
 
-    let items = [];
+    //Endast för test, hela stycket ska bort sen.
+    const mockitems = [
+        { id: 100101, category: "camera", brand: "Canon", model: "EOS R100", price: 4999, quantity: 1 },
+        { id: 100102, category: "camera", brand: "Nikon", model: "Z6 II", price: 6899, quantity: 1 },
+        { id: 100103, category: "camera", brand: "Sony", model: "A7 V", price: 3599, quantity: 1 },
+    ];
+
+    //För test, senare ändra till items = [];
+    let items = mockitems;
     let errorMessage = null;
 
     try {
@@ -38,9 +46,6 @@ export default async function CartPage() {
             <div className="max-w-7xl w-full mx-auto">
                 <h1 className="text-3xl font-bold mb-6">Kundkorg</h1>
                 <div className="flex-1 flex flex-col gap-6">
-                    <div className="p-4 bg-white shadow rounded w-full">Item 1</div>
-                    <div className="p-4 bg-white shadow rounded w-full">Item 2</div>
-                    <div className="p-4 bg-white shadow rounded w-full">Item 3</div>
                     {items.length === 0 ? (
                         <div className="p-4 bg-white shadow rounded w-full text-gray-600">Din varukorg är tom.</div>
                     ) : (
