b76d0011e490932b54a9da86847b3764e4934408
RebeccaLundberg1 <rebecca93lundberg@gmail.com>
2026-01-21 11:09:28 +0100

    Built a start in CartPage where three hardcoded items just for something to see, minor changes in layout and page: header hight based on width, darkmode color offwhite

--- frontend/app/cart/page.jsx
+++ frontend/app/cart/page.jsx
@@ -1,7 +1,20 @@
-export default function CartPage({ items }) {
+import CartItem from "@/app/cart/Item";
+
+export default function CartPage() {
     return (
-        <div className="p-6 max-w-3xl mx-auto">
-            <h1 className="text-3xl font-bold mb-6">Kundkorg</h1>
+        <div className="flex flex-col min-h-screen bg-zinc-50 font-sans dark:bg-zinc-50 p-6">
+            <div className="max-w-7xl w-full mx-auto">
+                <h1 className="text-3xl font-bold mb-6">Kundkorg</h1>
+                <div className="flex-1 flex flex-col gap-6">
+                    <div className="p-4 bg-white shadow rounded w-full">Item 1</div>
+                    <div className="p-4 bg-white shadow rounded w-full">Item 2</div>
+                    <div className="p-4 bg-white shadow rounded w-full">Item 3</div>
+                    {/*items*/}
+                </div>
+                <p className="mt-6">
+                    Här under kommer sedan totalt pris, knapp för lägg order etc.
+                </p>
+            </div>
         </div>
     );
 }
