cc0fca64499c4f785f714c9da778a04d9767452c
RebeccaLundberg1 <rebecca93lundberg@gmail.com>
2026-01-21 12:01:12 +0100

    Added try catch in CartPage like the one in Products, if-statment comment out to be able to see something else than error until backend is set up

--- frontend/app/cart/page.jsx
+++ frontend/app/cart/page.jsx
@@ -1,6 +1,38 @@
 import CartItem from "@/app/cart/Item";
+import ProductCard from "@/app/components/productcard";
 
-export default function CartPage() {
+export default async function CartPage() {
+    const url = `${process.env.BACKEND_API_URL}/cart`;
+
+    let items = [];
+    let errorMessage = null;
+
+    try {
+        const response = await fetch(url, {cache: "no-store" });
+
+        if(!response.ok) {
+            errorMessage = `Products API failed: ${response.status} ${response.statusText}`;
+        } else {
+            const data = await response.json();
+            items = Array.isArray(data) ? data : [];
+        }
+    } catch (err) {
+        errorMessage = "Service is currently unreachable.";
+    }
+
+    /*if (errorMessage) {
+        return (
+            <div className="flex flex-col min-h-screen bg-zinc-50 font-sans dark:bg-zinc-50 p-6">
+                <div className="max-w-7xl w-full mx-auto">
+                    <h1 className="text-3xl font-bold mb-6">Kundkorg</h1>
+                    <div className="rounded-md border border-red-200 bg-red-50 p-4 text-red-800">
+                        {errorMessage}
+                    </div>
+                </div>
+            </div>
+        );
+    }*/
+
     return (
         <div className="flex flex-col min-h-screen bg-zinc-50 font-sans dark:bg-zinc-50 p-6">
             <div className="max-w-7xl w-full mx-auto">
@@ -9,7 +41,13 @@ export default function CartPage() {
                     <div className="p-4 bg-white shadow rounded w-full">Item 1</div>
                     <div className="p-4 bg-white shadow rounded w-full">Item 2</div>
                     <div className="p-4 bg-white shadow rounded w-full">Item 3</div>
-                    {/*items*/}
+                    {items.length === 0 ? (
+                        <div className="p-4 bg-white shadow rounded w-full text-gray-600">Din varukorg är tom.</div>
+                    ) : (
+                        items.map((item) => (
+                            <CartItem key={item.id} item={item} />
+                        ))
+                    )}
                 </div>
                 <p className="mt-6">
                     Här under kommer sedan totalt pris, knapp för lägg order etc.
