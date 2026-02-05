864557a755ee3650599b9a97ab1585d69ce16112
RebeccaLundberg1 <rebecca93lundberg@gmail.com>
2026-02-05 14:52:13 +0100

    Carousel working and placed in productPage

--- frontend/app/products/[productId]/page.jsx
+++ frontend/app/products/[productId]/page.jsx
@@ -1,6 +1,7 @@
 import {getServerBaseUrl} from "@/app/utils/serverBaseUrl";
 import Image from "next/image";
 import Actions from "@/app/products/Actions"
+import Carousel from "@/app/products/Carousel";
 
 export default async function ProductPage({params}) {
     const resolvedParams = await params;
@@ -42,21 +43,10 @@ export default async function ProductPage({params}) {
     return (
         <div className="flex flex-col min-h-screen bg-[#e3e9e9] font-sans dark:bg-[#e3e9e9] p-6">
             <div className="max-w-3xl w-full mx-auto">
-                {/*<PictureCarousel(product.productId, product.category)/>*/}
-                {/* Följande div med bilden är bara för att visa på ett ungefärligt exempel när carousel är klar */}
-                <div className="relative w-full aspect-7/5">
-                    <Image
-                        src={
-                            product.category && product.id
-                                ? `/product-images/${encodeURIComponent(product.category)}/${encodeURIComponent(product.id)}/1.jpg`
-                                : "/product-images/camera.jpg"
-                        }
-                        alt={"Product"}
-                        fill
-                        className="object-cover"
-                        unoptimized
-                    />
-                </div>
+                <Carousel
+                    productId={product.id}
+                    category={product.category}
+                />
                 <h1 className="text-3xl font-bold mt-6 mb-6">{product.brand} {product.model} </h1>
                 <p className="font-normal">{product.description}</p>
                 <h2 className="text-2xl font-bold mt-6 mb-6">Pris: {product.price} SEK</h2>
